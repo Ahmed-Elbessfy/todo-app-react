f6880e9019b3c8e8315898ae7e888bbac741754c
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-01-23 10:17:49 +0200

    add todos component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './components/layout/Header'
+import Header from './components/layout/Header';
+import Todos from './components/todos/Todos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Todos />
       </div>
     );
   }
---
+++ src/components/todos/Todos.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+Todos.propTypes = {
+
+};
+
+export default Todos;
