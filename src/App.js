c86c2dbcb44eec1d0935d7a151311eeaf1a68b71
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-01-23 10:23:47 +0200

    add constructor  & state & uuid to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 import Header from './components/layout/Header';
 import Todos from './components/todos/Todos';
 
+import uuid from 'uuid';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todolist : [
+        {
+          id:uuid.v4(),
+          title: 'task 1',
+          complete: false
+        },
+        {
+          id:uuid.v4(),
+          title: 'task 2',
+          complete: false
+        },
+        {
+          id:uuid.v4(),
+          title: 'task 3',
+          complete: false
+        },
+        {
+          id:uuid.v4(),
+          title: 'task 4',
+          complete: false
+        },
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
