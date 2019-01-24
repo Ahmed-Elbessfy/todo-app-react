dbc94f4b9d082ae9f763b00da0885243dcceb548
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-01-24 07:16:40 +0200

    add API with axios package to handle Get & Post  & Delete request

--- src/App.js
+++ src/App.js
@@ -5,32 +5,33 @@ import Header from './components/layout/Header';
 import Todos from './components/todos/Todos';
 import Addtask from './components/todos/Addtask';
 
-import uuid from 'uuid';
+// import uuid from 'uuid';
+import axios from 'axios';
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
       todolist : [
-        {
-          id:uuid.v4(),
-          title: 'task 1',
-          complete: false
-        },
-        {
-          id:uuid.v4(),
-          title: 'task 2',
-          complete: false
-        },
-        {
-          id:uuid.v4(),
-          title: 'task 3',
-          complete: false
-        },
-        {
-          id:uuid.v4(),
-          title: 'task 4',
-          complete: false
-        },
+        // {
+        //   id:uuid.v4(),
+        //   title: 'task 1',
+        //   complete: false
+        // },
+        // {
+        //   id:uuid.v4(),
+        //   title: 'task 2',
+        //   complete: false
+        // },
+        // {
+        //   id:uuid.v4(),
+        //   title: 'task 3',
+        //   complete: false
+        // },
+        // {
+        //   id:uuid.v4(),
+        //   title: 'task 4',
+        //   complete: false
+        // },
       ]
     }
     this.toggleTask = this.toggleTask.bind(this);
@@ -38,6 +39,10 @@ class App extends Component {
     this.addNewTask = this.addNewTask.bind(this);
   }
 
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res  => this.setState({todolist : res.data}))
+  }
   //Toggle completing task status
   toggleTask(id){
     // console.log(id)
@@ -54,21 +59,15 @@ class App extends Component {
   //Delete task
   delTask(id){
     // console.log(id)
-    this.setState({
-      todolist : [...this.state.todolist.filter(task => task.id !== id)]
-    })
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res => this.setState({todolist : [...this.state.todolist.filter(task => task.id !== id)]}))
   }
 
   //Add new task
   addNewTask(title){
-    // console.log(title)    
-    this.setState({ 
-      todolist : [...this.state.todolist, {
-        id: uuid.v4(),
-        title,
-        complete: false
-      }]
-    })
+    // console.log(title)
+    axios.post('https://jsonplaceholder.typicode.com/todos',{title , complete: false})
+    .then(res => this.setState({todolist : [...this.state.todolist, res.data]}))
   }
   render() {
     return (
