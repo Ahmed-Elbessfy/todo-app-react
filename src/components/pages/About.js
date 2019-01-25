00653e402736935464f8c89898b816238b8c6d29
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-01-25 08:38:30 +0200

    edit and style About component
    add author portfolio link
    style the content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This a To-Do app using React.js. You can add,check and delete tasks.</p>
+    <div style = {{padding:'1rem'}} >
+      <h1 style={{paddingBottom:'2rem', textTransform:'uppercase'}}>about page</h1>
+      <p>This a To-Do app using React.js. You can add,check and delete tasks.
+        Created by : <a href='https://ahmed-elbessfy.github.io/Ahmed_Elbessfy_Portfolio/' target='_blank' rel='noopener noreferrer' style={{color:'#14416f', fontStyle:'italic',textDecoration: '#791414 underline wavy' , textTransform : 'uppercase'}}>Ahmed Elbessfy</a>
+      </p>
+      
     </div>
   )
 }
